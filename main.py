b648e36cc33e2078575fcd437c0837523c82666f
gdread3 <gdread3@gmail.com>
2025-02-24 11:33:07 -0700

    Added basic Gameloop
    addtionaly add code to allow the windows close button to work

--- main.py
+++ main.py
@@ -5,9 +5,26 @@ import pygame
 from constants import *
 
 def main():
+    if pygame.get_init() == False:
+        pygame.init()
     print ("Starting Asteroids!")
     print (f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    #basic Game loop
+    while True:
+        #quit option - makes close window button work
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        #fill Screen
+        screen.fill("black")
+
+
+        #refresh screen
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
